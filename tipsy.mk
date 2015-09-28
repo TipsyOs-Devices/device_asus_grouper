f9a616034c6d7bd6eb72f9a2a596ddd9a61b069e
slebit <tillnagotannat@gmail.com>
2015-09-28 19:29:43 +0200

    Use bootanimation with lower resolution

--- tipsy.mk
+++ tipsy.mk
@@ -25,3 +25,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
+		
+# Bootanimation		
+PRODUCT_COPY_FILES += \		
+	device/asus/grouper/prebuilt/bootanimation.zip:system/media/bootanimation.zip
